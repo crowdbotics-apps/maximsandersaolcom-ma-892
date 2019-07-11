e30261b26d3e312bc645d4d5421325debe77f53c
stefancrowdbotics <stefan@crowdbotics.com>
2019-07-12 00:17:00 +0200

    added fbsdk into project for both platform (login)

--- android/app/src/main/java/com/maximsandersaolcom_ma_892/MainApplication.java
+++ android/app/src/main/java/com/maximsandersaolcom_ma_892/MainApplication.java
@@ -3,6 +3,8 @@ package com.maximsandersaolcom_ma_892;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.facebook.reactnative.androidsdk.FBSDKPackage;
+import com.facebook.CallbackManager;
 import com.brentvatne.react.ReactVideoPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.facebook.react.ReactNativeHost;
@@ -14,6 +16,12 @@ import java.util.Arrays;
 import java.util.List;
 
 public class MainApplication extends Application implements ReactApplication {
+  private static CallbackManager mCallbackManager = CallbackManager.Factory.create();
+  
+  protected static CallbackManager getCallbackManager() {
+        return mCallbackManager;
+    }
+  
 
   private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
     @Override
@@ -25,6 +33,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new FBSDKPackage(mCallbackManager),
             new ReactVideoPackage(),
             new RNGestureHandlerPackage()
       );
