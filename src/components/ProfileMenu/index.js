5ddd36077f80ee07f3dba2b94531025b4d485420
stefancrowdbotics <stefan@crowdbotics.com>
2019-07-16 20:20:44 +0200

    added icons into project

--- src/components/ProfileMenu/index.js
+++ src/components/ProfileMenu/index.js
@@ -7,35 +7,39 @@ import ProfileMenuItem from '../ProfileMenuItem';
 import Routes from '../../Routes';
 import i18n from '../../i18n/i18n';
 
-const menuItemIconPlaceholder = require('../../assets/Icon_user.png');
+const findMyProgramIcon = require('../../assets/icon_find_my_program.png');
+const myFavoritesIcon = require('../../assets/icon_my_favorites.png');
+const myProgressAndDataIcon = require('../../assets/icon_my_progress.png');
+const learnIcon = require('../../assets/icon_learn.png');
+const settingsIcon = require('../../assets/icon_settings.png');
 
 const ProfileMenu = () => (
   <View style={{ width: '100%', marginTop: 20, marginBottom: 300 }}>
     <ScrollView>
       <ProfileMenuItem
-        menuIcon={menuItemIconPlaceholder}
+        menuIcon={findMyProgramIcon}
         menuText={i18n.t('profileScreen.profileMenuItems.findMyProgram')}
         route={Routes.TestScreen}
       />
       <ProfileMenuItem
-        menuIcon={menuItemIconPlaceholder}
+        menuIcon={myFavoritesIcon}
         menuText={i18n.t('profileScreen.profileMenuItems.myFavorites')}
         route={Routes.TestScreen}
       />
       <ProfileMenuItem
-        menuIcon={menuItemIconPlaceholder}
+        menuIcon={myProgressAndDataIcon}
         menuText={i18n.t('profileScreen.profileMenuItems.myProgressAndData')}
         route={Routes.TestScreen}
       />
       <ProfileMenuItem
-        menuIcon={menuItemIconPlaceholder}
+        menuIcon={learnIcon}
         menuText={i18n.t('profileScreen.profileMenuItems.learn')}
         route={Routes.TestScreen}
       />
       <ProfileMenuItem
-        menuIcon={menuItemIconPlaceholder}
+        menuIcon={settingsIcon}
         menuText={i18n.t('profileScreen.profileMenuItems.settings')}
-        route={Routes.TestScreen}
+        openDrawer
       />
     </ScrollView>
   </View>
