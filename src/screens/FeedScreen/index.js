47c100a6f01ae156a188745d7410878641e12eda
koshykmaksym <koshykmaksym@gmail.com>
2020-04-05 19:10:26 +0300

    fixed feed

--- src/screens/FeedScreen/index.js
+++ src/screens/FeedScreen/index.js
@@ -1,19 +1,20 @@
 import React from 'react';
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet, SafeAreaView } from 'react-native';
 import FeedContainer from '../../containers/FeedContainer';
-import { isIphoneX } from '../../utils/common';
+
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    flexDirection: 'column',
-    backgroundColor: 'white',
-    marginTop: isIphoneX() ? 30 : 0
-  }
+    container: {
+        flex: 1,
+        flexDirection: 'column',
+        backgroundColor: 'white',
+    }
 });
 const FeedScreen = () => (
-  <View style={styles.container}>
-    <FeedContainer />
-  </View>
+    <SafeAreaView style={{ flex: 1 }}>
+        <View style={styles.container}>
+            <FeedContainer/>
+        </View>
+    </SafeAreaView>
 );
 export default FeedScreen;
